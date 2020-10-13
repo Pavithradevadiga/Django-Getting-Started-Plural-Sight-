eb0e95d2c164bd7b3e5e5cd862575ca8fe5de174
Pavithra <pavithradevadiga1@gmail.com>
2020-10-13 11:12:25 +0530

    Time and duration is added with default field

--- MeetingsTable/MeetingAllocation/models.py
+++ MeetingsTable/MeetingAllocation/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from datetime import time
 
 # Create your models here.
 class Meeeting(models.Model):
     Title = models.CharField(max_length=20)
     Date = models.DateField()
+    Time = models.TimeField(default=time(9))
+    Duration = models.IntegerField(default=1)
