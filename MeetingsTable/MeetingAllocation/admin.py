b9edbec0f350a6f5953bc09266e7e22a79d99276
Pavithra <pavithradevadiga1@gmail.com>
2020-10-13 12:00:42 +0530

    Foreign Key is created to link between Meeting and Room

--- MeetingsTable/MeetingAllocation/admin.py
+++ MeetingsTable/MeetingAllocation/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Meeeting
+from .models import Meeeting,Room
 
 # Register your models here.
 admin.site.register(Meeeting)
+admin.site.register(Room)
--- MeetingsTable/MeetingAllocation/models.py
+++ MeetingsTable/MeetingAllocation/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from datetime import time
 
 # Create your models here.
-class Meeeting(models.Model):
-    Title = models.CharField(max_length=20)
-    Date = models.DateField()
-    Time = models.TimeField(default=time(9))
-    Duration = models.IntegerField(default=1)
-
-    def __str__(self):
-        return f"{self.Title} will be held on {self.Date} at {self.Time} for {self.Duration} hour/s"    
 
 class Room(models.Model):
     Name = models.CharField(max_length=20)
@@ -17,4 +9,17 @@ class Room(models.Model):
     No = models.IntegerField()
 
     def __str__(self):
-        return f"Meeting is being held in the floor {self.Floor} :{self.no} {self.Name}"     
+        return f"Meeting will be held in the Floor:{self.Floor} RoomNo: {self.No} Name:{self.Name}"          
+ 
+
+class Meeeting(models.Model):
+    Title = models.CharField(max_length=20)
+    Date = models.DateField()
+    Time = models.TimeField(default=time(9))
+    Duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.Title} will be held on {self.Date} at {self.Time} for {self.Duration} hour/s"    
+
+
