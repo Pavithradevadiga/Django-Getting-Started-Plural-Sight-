115a82a6fbbd56171d2096f16626222c00d5accd
Pavithra <pavithradevadiga1@gmail.com>
2020-10-14 11:15:07 +0530

    Templates are created along with dynamic template holder

--- MeetingsTable/Meetings/views.py
+++ MeetingsTable/Meetings/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def Welcome(request):
-    return HttpResponse("Welcome to the page")
+    return render(request,"Meetings/welcome.html",
+    {"message" : "To check if template variable is working fine"}
+    )
 
 def About(request):
     return HttpResponse("An effective meeting planner")    
