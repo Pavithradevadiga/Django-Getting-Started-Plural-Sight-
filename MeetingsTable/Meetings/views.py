2babb5f44ff2bdb414b838095ea7ade09ec8e87b
Pavithra <pavithradevadiga1@gmail.com>
2020-10-15 10:47:11 +0530

    Changes to dynamic template holder

--- MeetingsTable/Meetings/views.py
+++ MeetingsTable/Meetings/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from MeetingAllocation.models import Meeeting
 
 # Create your views here.
 def Welcome(request):
     return render(request,"Meetings/welcome.html",
-    {"message" : "To check if template variable is working fine"}
-    )
-
+    {"message" : Meeeting.objects.count()})
 def About(request):
     return HttpResponse("An effective meeting planner")    
