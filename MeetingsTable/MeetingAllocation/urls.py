fd2540c4f1ffbb64e7074c8a6f92b57d45d16fcf
Pavithra <pavithradevadiga1@gmail.com>
2020-10-24 15:45:14 +0530

    Creating a form with submit button

--- MeetingsTable/MeetingAllocation/urls.py
+++ MeetingsTable/MeetingAllocation/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<int:id>',views.detail,name = "detail"),
-    path('room',views.roomdetail,name="roomdetail")
+    path('room',views.roomdetail,name="roomdetail"),
+    path('newroom',views.new,name="new")
 ]
--- MeetingsTable/MeetingAllocation/views.py
+++ MeetingsTable/MeetingAllocation/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.forms import modelform_factory
 
 # Create your views here.
 from .models import Meeeting,Room
@@ -9,3 +10,9 @@ def detail(request,id):
 
 def roomdetail(request):
     return render(request,"MeetingAllocation/roomdetail.html",{"rooms":Room.objects.all()})
+
+MeetingForm = modelform_factory(Meeeting,exclude=[])
+
+def new(request):
+    form = MeetingForm()
+    return render(request,'MeetingAllocation/new.html',{"form": form})
